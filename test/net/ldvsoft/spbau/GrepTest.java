24c0ddb02ded0bd4fc55c160fd823e3be7d716f3
LDVSOFT <lapshin.dv@mail.ru>
2016-12-21 12:11:30 +0300

    Added more tests for Grep

--- test/net/ldvsoft/spbau/GrepTest.java
+++ test/net/ldvsoft/spbau/GrepTest.java
@@ -2,6 +2,11 @@ package net.ldvsoft.spbau;
 
 import org.junit.Test;
 
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 /**
  * Tests for grep
  */
@@ -9,5 +14,21 @@ public class GrepTest {
     @Test
     public void testSimple() {
         ShellTestUtils.doTest("echo 1 | grep 1\nexit", "1\n");
+    }
+
+    @Test
+    public void testFileAndAppend() throws IOException {
+        Path tmp = Files.createTempFile("test", ".tmp");
+        new PrintStream(Files.newOutputStream(tmp)).print("1\n2\n3\n4\n5\n6\n");
+        ShellTestUtils.doTest("grep -A 2 '3' " + tmp + "\nexit", "3\n4\n5\n--\n");
+    }
+
+    @Test
+    public void testIgnoreCase() throws IOException {
+        ShellTestUtils.doTest("echo 1\n" +
+                "echo 'HeLlO WoRlD' | grep -i 'hello world'\n" +
+                "echo 2\n" +
+                "echo 'HeLlO WoRlD' | grep 'hello world'\n" +
+                "exit", "1\nHeLlO WoRlD\n2\n");
     }
 }
