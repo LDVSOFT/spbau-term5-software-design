cd9be6c2694a8d27bf5dfc2b125c8b195f5c4d0f
LDVSOFT <lapshin.dv@mail.ru>
2016-12-23 19:38:24 +0300

    Added more tests

--- test/net/ldvsoft/spbau/GrepTest.java
+++ test/net/ldvsoft/spbau/GrepTest.java
@@ -19,8 +19,8 @@ public class GrepTest {
     @Test
     public void testFileAndAppend() throws IOException {
         Path tmp = Files.createTempFile("test", ".tmp");
-        new PrintStream(Files.newOutputStream(tmp)).print("1\n2\n3\n4\n5\n6\n");
-        ShellTestUtils.doTest("grep -A 2 '3' " + tmp + "\nexit", "3\n4\n5\n--\n");
+        new PrintStream(Files.newOutputStream(tmp)).print("1\n2\n3\n4\n5\n6\n98\n99\n100");
+        ShellTestUtils.doTest("grep -A 2 '3|99' " + tmp + "\nexit", "3\n4\n5\n--\n99\n100\n--\n");
     }
 
     @Test
@@ -30,5 +30,23 @@ public class GrepTest {
                 "echo 2\n" +
                 "echo 'HeLlO WoRlD' | grep 'hello world'\n" +
                 "exit", "1\nHeLlO WoRlD\n2\n");
+    }
+
+    @Test
+    public void testRegex1() throws IOException {
+        Path tmp = Files.createTempFile("test", ".tmp");
+        String lines = "" +
+                "+79 01 000 0 0 00\n" +
+                "+70919999999\n" +
+                "+7 (156) 653-42-65\n" +
+                "+7093 546 3566\n" +
+                "+7-567-56-66-335";
+        String result = "" +
+                "+70919999999\n" +
+                "+7 (156) 653-42-65\n" +
+                "+7093 546 3566\n";
+        new PrintStream(Files.newOutputStream(tmp)).print(lines);
+        ShellTestUtils.doTest("grep '\\+7 ?(?:[0-9]{3}|\\([0-9]{3}\\)) ?[0-9]{3}[ -]?[0-9]{2}[ -]?[0-9]{2}' "
+                + tmp + "\nexit", result);
     }
 }
